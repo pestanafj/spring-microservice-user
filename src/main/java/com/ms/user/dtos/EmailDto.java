a7e4a5bd69f6a1c03e0f1b01be9dc0e5904dbeca
Fernanda Pestana <pestanafj@gmail.com>
2024-05-02 23:43:34 -0300

    Configurado o envio do email

---
+++ src/main/java/com/ms/user/dtos/EmailDto.java
@@ -0,0 +1,52 @@
+package com.ms.user.dtos;
+
+import java.util.UUID;
+
+public class EmailDto {
+
+    private UUID userID;
+    private String name;
+    private String emailTo;
+    private String subject;
+    private String text;
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getEmailTo() {
+        return emailTo;
+    }
+
+    public void setEmailTo(String emailTo) {
+        this.emailTo = emailTo;
+    }
+}
